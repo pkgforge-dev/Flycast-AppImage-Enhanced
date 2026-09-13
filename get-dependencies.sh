3384d5847746dbf2d41084b045913e9a27ffdbda
Link4Electronics <link4electronics@gmail.com>
2026-09-13 01:06:23 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,17 +7,48 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-    clang \
-    sdl2-compat
+	cmake \
+    sdl2-compat \
+	alsa-lib \
+  freetype2 \
+  glslang \
+  hicolor-icon-theme \
+  libao \
+  libcdio \
+  libgomp \
+  libjuice \
+  libpulse \
+  libzip \
+  lua \
+  miniupnpc \
+  systemd-libs \
+  vulkan-headers \
+  zlib
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano libdecor-mini ! llvm
 
+#if [ "${DEVEL_RELEASE-}" = 1 ]; then
+#	package=flycast-git
+#else
+#	package=flycast
+#fi
+#make-aur-package "$package"
+#pacman -Q "$package" | awk '{print $2; exit}' > ~/version
+
+echo "Building Flycast..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/flyinghead/flycast"
 if [ "${DEVEL_RELEASE-}" = 1 ]; then
-	package=flycast-git
+    echo "Making nightly build of Flycast..."
+    echo "---------------------------------------------------------------"
+    VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+    git clone --depth 1 "$REPO" ./flycast
 else
-	package=flycast
+	echo "Making stable build of Flycast..."
+	VERSION="$(git ls-remote --tags --sort="v:refname" "$REPO" | tail -n1 | sed 's/.*\///; s/\^{}//; s/^v//')"
+	git clone --branch v"$VERSION" --single-branch --depth 1 "$REPO" ./flycast
 fi
-make-aur-package "$package"
-pacman -Q "$package" | awk '{print $2; exit}' > ~/version
+echo "$VERSION" > ~/version
+
